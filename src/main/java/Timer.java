e61186b5ffe66ba207e0c915963f305cdabd590a
Liliacea <Liliacea_85@mail.ru>
2023-03-25 22:02:59 +0300

    edits+timer

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -5,12 +5,43 @@ public class Timer extends SimpleArrayList {
         SimpleArrayList<Integer> arrayList;
         values = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         arrayList = new SimpleArrayList<>();
-        for (int i = 0; i < 600; i++) {
+        for (int i = 0; i < 1200; i++) {
             arrayList.add(i);
         }
+        long startRemoveCenter = System.nanoTime();
+        arrayList.remove(600);
+        System.out.println(System.nanoTime()-startRemoveCenter);
 
-        long m = System.currentTimeMillis();
+        long startDeleteCenter = System.nanoTime();
+        arrayList.delete(600);
+        System.out.println(System.nanoTime()-startDeleteCenter);
 
-        System.out.println(m);
+        long startRemoveEnd = System.nanoTime();
+        arrayList.remove(1200);
+        System.out.println(System.nanoTime() - startRemoveEnd);
+
+        long startDeleteEnd = System.nanoTime();
+        arrayList.delete(1200);
+        System.out.println(System.nanoTime()-startDeleteEnd);
+
+        long startRemoveTwoThirds = System.nanoTime();
+        arrayList.remove(800);
+        System.out.println(System.nanoTime() - startRemoveTwoThirds);
+
+        long startDeleteTwoThirds = System.nanoTime();
+        arrayList.delete(800);
+        System.out.println(System.nanoTime() - startDeleteTwoThirds);
+
+        long startRemoveFirst = System.nanoTime();
+        arrayList.remove(1);
+        System.out.println(System.nanoTime() - startRemoveFirst);
+
+        long startDeleteFirst = System.nanoTime();
+        arrayList.delete(800);
+        System.out.println(System.nanoTime() - startDeleteFirst);
+
     }
+
+
+
 }
--- src/test/java/SimpleArrayListTest.java
+++ src/test/java/SimpleArrayListTest.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
